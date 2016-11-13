cbafac7a0d04a318472c1694f50a9b61721b4d6a
cadonten <cardmechanic@gmx.de>
2016-11-13 19:26:55 +0100

    Add files via upload

--- Physics.js
+++ Physics.js
@@ -5,18 +5,25 @@ var Const = {
 	PLANE : 1
  };
 
-function PhysObj ( mesh, stc, typus ){
+function PhysObj ( mesh, stc, typus, bound ){
 	
 	this.typus = typus;
-	this.bound = 1;
-	this.stc = stc;
+	this.bound = bound;
 	this.mesh = mesh;
+	this.stc = stc;
 	this.vel = new THREE.Vector3();
 	this.acc = new THREE.Vector3();
 	this.phi = new THREE.Vector3();
 	this.dphi = new THREE.Vector3();
 	this.ddphi = new THREE.Vector3();
 	this.mass = 1;
+	
+	if ( this.typus == 0){
+		this.bound = this.mesh.geometry.parameters.radius;
+	}
+	else if ( this.typus == 1){
+		this.bound = Math.abs(this.mesh.geometry.vertices[0].x);
+	}
 }
 
 PhysObj.prototype = {
@@ -28,7 +35,7 @@ PhysObj.prototype = {
 		
 		this.mesh.position.add(dp);
 		this.vel.add(dv);
-	},
+	}, 
 	accel : function(dt){
 		
 		this.acc.set (0, -.8*this.mesh.position.y, 0);
@@ -40,7 +47,7 @@ PhysObj.prototype = {
 	},
 	intrscBound : function ( physObj ){
 				
-		return this.mesh.position.clone( ).sub( physObj.mesh.position ).length() < this.bound + physObj.bound;
+		return this.mesh.position.clone( ).sub( physObj.mesh.position ).length() < this.bound + physObj.bound  >.0001;
 	},
 	intrs : function( physObj ){
 		
@@ -53,22 +60,26 @@ PhysObj.prototype = {
 		
 			case Const.SPHERE : {
 					
-				return this.intrsSphere( physObj );
+				if ( this.intrsSphere( physObj ) ){
+					this.SpheretoPlane( physObj );
+					return;
+				}
 			}
 			
-			case Const.PLANE: {
+			case Const.PLANE : {
 			
-				return this.intrsPlane( physObj );
+				//return this.intrsPlane( physObj );
 			}
 			default : return false;
 		}
 		return false;
-	}
+	},
+	
 };
 
-function PhysSphere ( mesh, stc ){
+function PhysSphere ( mesh, stc){
 	
-	PhysObj.call (this, mesh, stc, Const.SPHERE );
+	PhysObj.call (this, mesh, stc, Const.SPHERE);
 }
 
 PhysSphere.prototype = new PhysObj;
@@ -83,16 +94,16 @@ PhysSphere.prototype.intrsSphere = function( physObj ){
 		}
 		case Const.PLANE: {
 			
-			var e = physObj.matrix.elements;
+			var e = physObj.mesh.matrix.elements;
 			var u = new THREE.Vector3( e[0], e[1], e[2] );
 			var v = new THREE.Vector3( e[4], e[5], e[6] );
 			var n = new THREE.Vector3( e[8], e[9], e[10] );
 			
-			u.multiplyScalar(Math.abs(physObj.geometry.vertices[0].x) );
-			v.multiplyScalar(Math.abs(physObj.geometry.vertices[0].y) );
+			u.multiplyScalar(Math.abs(physObj.mesh.geometry.vertices[0].x) );
+			v.multiplyScalar(Math.abs(physObj.mesh.geometry.vertices[0].y) );
 			var dist = physObj.mesh.position.clone().sub(this.mesh.position);
 			var c = n.dot(dist);
-			if (c > this.parameters.radius)
+			if (Math.abs(c) > this.mesh.geometry.parameters.radius || c == 0)
 				return false;
 			
 			var a = u.dot(dist)/u.dot(u);
@@ -105,6 +116,16 @@ PhysSphere.prototype.intrsSphere = function( physObj ){
 		}
 		default : break;
 	}
+}
+
+PhysSphere.prototype.SpheretoPlane = function ( physObj ){
+	
+	var e = physObj.mesh.matrix.elements;
+	var n = new THREE.Vector3( e[8], e[9], e[10] ).normalize();
+	
+	var scale = this.vel.dot(n);
+	n.multiplyScalar(-2*scale);
+	this.vel.add(n);
 }
 
 function PhysPlane ( mesh, stc ){
@@ -153,6 +174,11 @@ Physics.prototype = {
 			
 			this.objs[i].move(dt);
 		}
+		for ( var i = 0; i < this.objs.length; i++ ){
+			for ( var j = i+1; j < this.objs.length; j++ ){
+			
+				this.objs[i].intrs( this.objs[j] );
+			}
+		}
 	}
-	
 }
